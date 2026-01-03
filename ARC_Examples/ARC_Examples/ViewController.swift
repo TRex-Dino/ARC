219c386c9dc496e83c35a43fa306cd88afd37a98
TRex-Dino <dmitrysinkov6@gmail.com>
2026-01-03 17:11:27 +0300

    create project

---
+++ ARC_Examples/ARC_Examples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ARC_Examples
+//
+//  Created by Menkov Dmitriy on 03.01.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
